673db9a2bc16c1a39cafe0248d85132436673899
danuruiz7 <danuruiz7@hotmail.com>
2024-07-22 17:47:44 +0200

    hola4

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -3,19 +3,24 @@ import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
 
 export async function POST(request: NextRequest) {
-  const data = await request.formData();
-  const file: File | null = data.get('file') as unknown as File;
+  try {
+    const data = await request.formData();
+    const file: File | null = data.get('file') as unknown as File;
 
-  if (!file) {
+    if (!file) {
+      return NextResponse.json({ success: false });
+    }
+
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const filePath = path.join(process.cwd(), 'public', file.name);
+    await writeFile(filePath, buffer);
+    console.log(`open ${filePath} to see the uploaded file`);
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error(error);
     return NextResponse.json({ success: false });
   }
-
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-
-  const filePath = path.join(process.cwd(), 'public', file.name);
-  await writeFile(filePath, buffer);
-  console.log(`open ${filePath} to see the uploaded file`);
-
-  return NextResponse.json({ success: true });
 }
